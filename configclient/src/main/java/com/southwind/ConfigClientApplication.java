aecf1378e1502ae484bb066e629ed28c1d957d81
lidonghui-github <1161656983@qq.com>
2020-03-04 13:28:22 +0800

    configserver远程配置

--- configclient/src/main/java/com/southwind/ConfigClientApplication.java
+++ configclient/src/main/java/com/southwind/ConfigClientApplication.java
@@ -2,11 +2,13 @@ package com.southwind;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by Administrator on 2020/3/4.
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class ConfigClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class,args);
