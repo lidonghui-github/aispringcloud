887f6a07d434051abb15795964b3ecf437cb3660
lidonghui-github <1161656983@qq.com>
2020-03-04 12:56:30 +0800

    configserver远程配置

---
+++ configclient/src/main/java/com/southwind/ConfigClientApplication.java
@@ -0,0 +1,14 @@
+package com.southwind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Administrator on 2020/3/4.
+ */
+@SpringBootApplication
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class,args);
+    }
+}
